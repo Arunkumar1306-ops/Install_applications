79832a00a91b1daa6db7d1ecdea1952e7a4c74e4
Arun <84366721+Arunkumar1306-ops@users.noreply.github.com>
2026-01-09 17:23:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+ 
+print("Python example 2 running ")
+ 
+current_time = datetime.datetime.now()
+print("Current date and time:", current_time)
+ 
+current_dir = os.getcwd()
+print("Current directory:", current_dir)
+ 
